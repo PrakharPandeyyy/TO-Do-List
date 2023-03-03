e76680f08860571416f9a9028fd89ba15e9a3c98
PrakharPandeyyy <8.prakharpandey@gmail.com>
2023-03-03 22:10:59 +0530

    updated few codes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const bodyparser=require("body-parser");
+const date=require(__dirname+"/date.js");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -7,30 +8,41 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 let items =[];
+let workItems=[];
 
 app.get("/",function(req,res){
 
-    let today=new Date();
-
-    let options = {
-        weekday : "long",
-        day : "numeric",
-        month: "long"
-    };
-
-    let day =today.toLocaleDateString("en-US",options);
+    let day=date();
 
     res.render("list",{
-        kindofday:day , newListItems :items });
+        listTitle:day , newListItems :items });
 
 
 });
 app.post("/",function(req,res){
     let item=req.body.newItem;
-    items.push(item);
+if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+}
+else{
+    
+        items.push(item); 
+        res.redirect("/");
 
-    res.redirect("/");
+}
 });
+
+app.get("/work",function(req,res){
+    res.render("list",{
+        listTitle:"Work",newListItems:workItems
+    })
+});
+app.post("/work",function(req,res){
+    let items=req.body.newItem;
+    workItems.push(items);
+});
+
 
 
 app.listen(3000,function(){
